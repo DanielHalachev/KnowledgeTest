ec51332b7c1a4e5aff67434603549dc2a8dbfcd6
DanielHalachev <dani22h09_pl@abv.bg>
2023-06-20 18:44:40 +0300

    Added custom user headers

---
+++ includes/guestUserHeader.php
@@ -0,0 +1,11 @@
+<header>
+  <nav>
+    <h1><a href="./index.php">KnowledgeTest</a></h1>
+    <ul>
+      <a href="./index.php"><li><span class="fa fa-house"></span>Начало</li></a>
+      <a href="./login.php"><li><span class="fa fa-right-to-bracket"></span>Вписване</li></a>
+      <a href="./register.php"><li><span class="fa fa-user-plus"></span>Регистрация</li></a>
+      <button onclick="toggleTheme()"><span class="fa fa-circle-half-stroke"></span></button>
+    </ul>
+  </nav>
+</header>
---
+++ includes/loggedInUserHeader.php
@@ -0,0 +1,13 @@
+<header>
+  <nav>
+    <span class="fa fa-bars" id="menu" onclick="toggleNav()"></span>
+    <h1><a href="./index.php">KnowledgeTest</a></h1>
+    <ul>
+      <a href="./home.php"><li><span class="fa fa-user"></span>Потребител</li></a>
+      <a href="./questions.php"><li><span class="fa fa-question"></span>Въпроси</li></a>
+      <a onclick="logout()"><li><span class="fa fa-power-off"></span>Изход</li></a>
+      <button onclick="toggleTheme()"><span class="fa fa-circle-half-stroke"></span></button>
+    </ul>
+  </nav>
+</header>
+
