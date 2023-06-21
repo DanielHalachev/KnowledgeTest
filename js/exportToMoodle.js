00481b12c1895665d8cfb8ca6f0d69503a4d0efc
Stefan Velev <sdvelev@gmail.com>
2023-06-21 22:37:51 +0300

    Changed name and added some new lines inside xml generated file

--- js/exportToMoodle.js
+++ js/exportToMoodle.js
@@ -35,62 +35,86 @@ function getAnswers(questionId) {
 
 function generateMoodleXML(questions, topic) {
     const xml = document.createElement('quiz');
+    xml.appendChild(document.createTextNode('\n'));
     const questionTopicElement = document.createElement('question');
-    questionTopicElement.setAttribute('type', 'category')
-    const questionCategoryElement = document.createElement('category');
-    const textElement = document.createElement('text');
-    textElement.textContent = topic;
+    questionTopicElement.appendChild(document.createTextNode('\n'));
+    questionTopicElement.setAttribute('type', 'category');
+    xml.appendChild(questionTopicElement);
 
+    const questionCategoryElement = document.createElement('category');
+    questionCategoryElement.appendChild(document.createTextNode('\n'));
+    const textElement = document.createElement('text');
+    textElement.appendChild(document.createTextNode('\n'));
+    textElement.textContent = topic;
     questionCategoryElement.appendChild(textElement);
     questionTopicElement.appendChild(questionCategoryElement);
     xml.appendChild(questionTopicElement);
+    xml.appendChild(document.createTextNode('\n'));
+
 
     questions.forEach(question => {
         const questionElement = document.createElement('question');
+        questionElement.appendChild(document.createTextNode('\n'));
         questionElement.setAttribute('type', 'multichoice');
+        xml.appendChild(questionElement);
 
         const name = document.createElement('name');
+        name.appendChild(document.createTextNode('\n'));
         const text = document.createElement('text');
+        text.appendChild(document.createTextNode('\n'));
         text.textContent = question.label;
         name.appendChild(text);
         questionElement.appendChild(name);
+        questionElement.appendChild(document.createTextNode('\n'));
 
         const questiontextElement = document.createElement('questiontext');
+        questiontextElement.appendChild(document.createTextNode('\n'));
         questiontextElement.setAttribute('format', 'plain_text');
         const questionText = document.createElement('text');
+        questionText.appendChild(document.createTextNode('\n'));
         questionText.textContent = question.label;
         questiontextElement.appendChild(questionText);
         questionElement.appendChild(questiontextElement);
+        questionElement.appendChild(document.createTextNode('\n'));
 
         const answernumberingElement = document.createElement('answernumbering');
+        answernumberingElement.appendChild(document.createTextNode('\n'));
         answernumberingElement.textContent = 'ABCD';
         questionElement.appendChild(answernumberingElement);
+        questionElement.appendChild(document.createTextNode('\n'));
 
         const numberOfCorrectAnswers = question.answers.filter(answer => answer.isCorrect === true).length;
 
         question.answers.forEach(answer => {
             const answerElement = document.createElement('answer');
+            answerElement.appendChild(document.createTextNode('\n'));
             answerElement.setAttribute('fraction', answer.isCorrect ? 100.0 / numberOfCorrectAnswers : 0);
             answerElement.setAttribute('format', 'plain_text');
             const answerText = document.createElement('text');
+            answerText.appendChild(document.createTextNode('\n'));
             answerText.textContent = answer.label;
             answerElement.appendChild(answerText);
 
             const feedback = document.createElement('feedback');
+            feedback.appendChild(document.createTextNode('\n'));
             const feedbackText = document.createElement('text');
+            feedbackText.appendChild(document.createTextNode('\n'));
             feedbackText.textContent = answer.isCorrect ? question.correctFeedback : question.incorrectFeedback;
             feedback.appendChild(feedbackText);
             answerElement.appendChild(feedback);
             questionElement.appendChild(answerElement);
+            questionElement.appendChild(document.createTextNode('\n'));
         });
 
         xml.appendChild(questionElement);
+        xml.appendChild(document.createTextNode('\n'));
     });
 
     const serializer = new XMLSerializer();
-    const xmlDeclaration = '<' + '?xml version="1.0" encoding="UTF-8"?>\n';
+    const xmlDeclaration = '<?xml version="1.0" encoding="UTF-8"?>\n';
     return xmlDeclaration + serializer.serializeToString(xml);
 }
+
 
 function exportToMoodleXML(testId, topic) {
     getQuestions(testId)
@@ -108,7 +132,7 @@ function exportToMoodleXML(testId, topic) {
                     const xmlString = generateMoodleXML(questions, topic);
                     const element = document.createElement('a');
                     element.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(xmlString));
-                    element.setAttribute('download', 'questions.xml');
+                    element.setAttribute('download', 'moodleXmlFormat.xml');
                     element.style.display = 'none';
                     document.body.appendChild(element);
                     element.click();
